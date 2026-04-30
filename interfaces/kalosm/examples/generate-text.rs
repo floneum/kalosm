ed570c8353b68b0293336afb6b8a0d9781d675c6
Evan Almloff <evanalmloff@gmail.com>
2026-04-29 20:26:16 -0500

    use a smaller model in generate text

--- interfaces/kalosm/examples/generate-text.rs
+++ interfaces/kalosm/examples/generate-text.rs
@@ -4,7 +4,7 @@ use kalosm::language::*;
 async fn main() {
     tracing_subscriber::fmt::init();
     let prompt = "The following is a 300 word essay about why the capital of France is Paris:";
-    let llm = Llama::new().await.unwrap();
+    let llm = Llama::builder().with_source(LlamaSource::qwen_3_0_6b_instruct()).build().await.unwrap();
 
     print!("{prompt}");
 
