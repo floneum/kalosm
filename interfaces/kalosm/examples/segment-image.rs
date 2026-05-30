4b9802d5f539c0ad329ee936e87de544a8ac45b7
Evan Almloff <evanalmloff@gmail.com>
2026-05-30 09:40:45 -0500

    fix segment image example

--- interfaces/kalosm/examples/segment-image.rs
+++ interfaces/kalosm/examples/segment-image.rs
@@ -1,15 +1,16 @@
 use kalosm::vision::*;
 
-#[tokio::main]
-async fn main() {
-    let model = SegmentAnything::builder().build().await.unwrap();
-    let image = image::open("examples/landscape.jpg").unwrap();
-    let images = model
-        .segment_from_points(
-            SegmentAnythingInferenceSettings::new(image).add_goal_point_normalized(0.5, 0.25),
-        )
-        .await
-        .unwrap();
+fn main() {
+    pollster::block_on(async {
+        let model = SegmentAnything::builder().build().await.unwrap();
+        let image = image::open("examples/landscape.jpg").unwrap();
+        let images = model
+            .segment_from_points(
+                SegmentAnythingInferenceSettings::new(image).add_goal_point_normalized(0.5, 0.25),
+            )
+            .await
+            .unwrap();
 
-    images.save("out.png").unwrap();
+        images.save("out.png").unwrap();
+    });
 }
